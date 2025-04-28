9392f863b8f2cfcc6b50e4c8811a723d512e97ab
Udit Bansal <Udit.ban2312@gmail.com>
2025-04-28 23:26:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http')
+const {Chess} = require('chess.js')
+
+ const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+let players = {};
+let currentplayer = "W"
