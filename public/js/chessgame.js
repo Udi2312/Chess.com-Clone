76705a975283e8d52f299b12095ef5e73afdeec3
Udit Bansal <Udit.ban2312@gmail.com>
2025-05-02 23:42:03 +0530

    commit

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -8,10 +8,47 @@ let playerRole = null;
 
 const renderBoard = () => {
     const board = chess.board();
-    boardElement.innerHTML = ''; 
+    
     board.forEach((row, rowIndex) => {
         row.forEach((square , squareIndex) =>{
-            
+            const squareElement = document.createElement('div');
+            squareElement.classList.add('square', (rowIndex + squareIndex) % 2 === 0 ? 'light' : 'black');
+            squareElement.dataset.row = rowIndex;
+            squareElement.dataset.col = squareIndex;
+            if(square){
+                const pieceElement = document.createElement('div');
+                pieceElement.classList.add('piece', square.color === 'w' ? 'white' : 'black');
+                pieceElement.innerText = "";
+                pieceElement.draggable = playerRole === square.color;
+
+                pieceElement.addEventListener('dragstart', (e) => {
+                    if(pieceElement.draggable){
+                        draggedPiece = pieceElement;
+                        souceSquare = {row: rowIndex, col: squareIndex};
+                        e.dataTransfer.setData('text/plain', '');
+                    }
+                })
+                pieceElement.addEventListener('dragend', (e) => {
+                    draggedPiece = null;
+                    souceSquare = null;
+                })
+
+                squareElement.appendChild(pieceElement);
+            }
+
+            squareElement.addEventListener('dragover', (e) => {
+                e.preventDefault();
+            })
+
+            squareElement.addEventListener('drop' , (e)=>{
+                e.preventDefault();
+                if(draggedPiece){
+                    const targetSquare = {row: parseInt(squareElement.dataset.row), 
+                        col: parseInt(squareElement.dataset.col)};
+                        handleMove(souceSquare, targetSquare);
+                }
+            })
+            boardElement.appendChild(squareElement);
         })
     }
 )
@@ -24,3 +61,5 @@ const handleMove = () => {
 const getPieceUnicode = () =>{
 
 }
+
+renderBoard();
